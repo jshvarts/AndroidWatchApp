506fd486ab77e6b7772bb8213333e8e5d8b0a557
James Shvarts <james.shvarts@viacom.com>
2016-06-08 16:58:28 -0400

    created layout with placeholder values

--- wear/src/main/java/com/example/android/sunshine/app/MainWearActivity.java
+++ wear/src/main/java/com/example/android/sunshine/app/MainWearActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -16,8 +17,9 @@ public class MainWearActivity extends WearableActivity {
             new SimpleDateFormat("HH:mm", Locale.US);
 
     private BoxInsetLayout mContainerView;
-    private TextView mTextView;
-    private TextView mClockView;
+    private TextView mTimeView;
+    private TextView mDateView;
+    private LinearLayout mTempView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +28,9 @@ public class MainWearActivity extends WearableActivity {
         setAmbientEnabled();
 
         mContainerView = (BoxInsetLayout) findViewById(R.id.container);
-        mTextView = (TextView) findViewById(R.id.text);
-        mClockView = (TextView) findViewById(R.id.clock);
+        mTimeView = (TextView) findViewById(R.id.time);
+        mDateView = (TextView) findViewById(R.id.date);
+        mTempView = (LinearLayout) findViewById(R.id.temp);
     }
 
     @Override
@@ -51,14 +54,13 @@ public class MainWearActivity extends WearableActivity {
     private void updateDisplay() {
         if (isAmbient()) {
             mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
-            mTextView.setTextColor(getResources().getColor(android.R.color.white));
-            mClockView.setVisibility(View.VISIBLE);
-
-            mClockView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+            mTimeView.setVisibility(View.VISIBLE);
+            mTimeView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
         } else {
             mContainerView.setBackground(null);
-            mTextView.setTextColor(getResources().getColor(android.R.color.black));
-            mClockView.setVisibility(View.GONE);
+            mTimeView.setTextColor(getResources().getColor(android.R.color.black));
+            mDateView.setVisibility(View.GONE);
+            mTempView.setVisibility(View.GONE);
         }
     }
 }
