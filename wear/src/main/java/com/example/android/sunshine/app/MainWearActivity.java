0421bb126566f2a9ca8c03ed5cb284da495caf8d
James Shvarts <james.shvarts@viacom.com>
2016-06-08 21:39:25 -0400

    setting date and time dynamically

--- wear/src/main/java/com/example/android/sunshine/app/MainWearActivity.java
+++ wear/src/main/java/com/example/android/sunshine/app/MainWearActivity.java
@@ -8,12 +8,16 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class MainWearActivity extends WearableActivity {
 
-    private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
+    private static final SimpleDateFormat DISPLAY_DATE_FORMAT =
+            new SimpleDateFormat("EEE, MMM d, yyyy", Locale.US);
+
+    private static final SimpleDateFormat DISPLAY_TIME_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
 
     private BoxInsetLayout mContainerView;
@@ -31,6 +35,7 @@ public class MainWearActivity extends WearableActivity {
         mTimeView = (TextView) findViewById(R.id.time);
         mDateView = (TextView) findViewById(R.id.date);
         mTempView = (LinearLayout) findViewById(R.id.temp);
+        setDateTime();
     }
 
     @Override
@@ -55,12 +60,18 @@ public class MainWearActivity extends WearableActivity {
         if (isAmbient()) {
             mContainerView.setBackgroundColor(getResources().getColor(android.R.color.black));
             mTimeView.setVisibility(View.VISIBLE);
-            mTimeView.setText(AMBIENT_DATE_FORMAT.format(new Date()));
+            setDateTime();
         } else {
             mContainerView.setBackground(null);
             mTimeView.setTextColor(getResources().getColor(android.R.color.black));
             mDateView.setVisibility(View.GONE);
             mTempView.setVisibility(View.GONE);
         }
+    }
+
+    private void setDateTime() {
+        Calendar cal = Calendar.getInstance(Locale.US);
+        mTimeView.setText(DISPLAY_TIME_FORMAT.format(cal.getTime()));
+        mDateView.setText(DISPLAY_DATE_FORMAT.format(cal.getTime()));
     }
 }
